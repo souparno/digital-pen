192f30b51c83fee5dd08da680c7385c880f2d768
bonnie <soparno.majumder@gmail.com>
2015-02-28 23:09:59 +0530

    jscs added for code beautify

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
-    qunit = require('node-qunit-phantomjs');
+    qunit = require('node-qunit-phantomjs'),
+    jscs = require('gulp-jscs');
 
+gulp.task('jscs', function () {
+    jscs('blackboard.js');
+});
 gulp.task('test', function() {
     qunit('./test/index.html');
 });
