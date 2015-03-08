027798fe62d5877cc2b7d94e4e8caff5438d6040
bonnie <soparno.majumder@gmail.com>
2015-03-09 03:03:38 +0530

    architecture restructured, and test cases arranged accordingly

--- assets/js/blackboard.js
+++ assets/js/blackboard.js
@@ -33,7 +33,7 @@ var Queue = (function () {
       action = new Action(x, y, type, interval);
       array.push(action);
     },
-    forEachpop: function (fn) {
+    forEachpop: function (fn, stackEmpty) {
       var iterate = function (i) {
         var action = temp_array.shift(),
           x = action.x,
@@ -42,17 +42,19 @@ var Queue = (function () {
           interval = action.interval;
 
         setTimeout(function () {
-          fn(x, y, type);
+          if (typeof fn === 'function') {
+            fn(x, y, type);
+          }
           if (--i) {
             iterate(i);
-          } else {
-            fn(undefined, undefined, 2);
+          } else if (typeof stackEmpty === 'function'){
+            stackEmpty();
           }
         }, interval);
       }, temp_array;
 
       temp_array = array.slice();
-      iterate(temp_array.length);      
+      iterate(temp_array.length);
     },
     get: function () {
       return array;
@@ -66,83 +68,53 @@ var Queue = (function () {
   });
 }());
 
+Pen = (function () {
+  var cntxt = null;
+
+  return Class.Create({
+    setContext: function (CNTXT) {
+      cntxt = CNTXT;
+    },
+    moveTo: function (x, y) {
+      cntxt.beginPath();
+      cntxt.moveTo(x, y);
+    },
+    lineTo: function (x, y) {
+      cntxt.lineTo(x, y);
+      cntxt.lineWidth = 2;
+      cntxt.strokeStyle = 'black';
+      cntxt.stroke();
+    }
+  });
+}());
+
 var Board = (function () {
   var canvas = null,
     cntxt = null,
     height = 0,
     width = 0,
-    minX = 0,
-    maxX = 0,
-    minY = 0,
-    maxY = 0,
-    Pen = (function () {
-      var getCoordinate = function (X, Y) {
-        var ratioX = width / (maxX - minX),
-          ratioY = height / (maxY - minY),
-          x = ((X + (minX < 0 ? (-1 * minX) : 0))* ratioX),
-          y = ((Y + (minY < 0 ? (-1 * minY) : 0)) * ratioY);
-
-        return {
-          x: x,
-          y: y
-        };
-      };
-
-      return Class.Create({
-        moveTo: function (x, y) {
-          var pos = getCoordinate(x, y);
-
-          cntxt.beginPath();
-          cntxt.moveTo(pos.x, pos.y);
-        },
-        lineTo: function (x, y) {
-          var pos = getCoordinate(x, y);
-
-          cntxt.lineTo(pos.x, pos.y);
-          cntxt.lineWidth = 2;
-          cntxt.strokeStyle = 'black';
-          cntxt.stroke();
-        }
-      });
-  }());
+    pen;
 
   return Class.Create({
-    init: function (elm, bound) {
-      var ratio = 0;
-
+    init: function () {
+      pen = new Pen();
+      this.pen = pen;
+    },
+    set: function (elm, ratio) {
       if (elm) {
         canvas = $(elm).get(0);
         cntxt = canvas.getContext('2d');
-        minX = bound.minX;
-        maxX = bound.maxX;
-        minY = bound.minY;
-        maxY = bound.maxY;
-        ratio = (maxY - minY) / (maxX - minX);        
         width = $(canvas).width();
         height = ratio * width;
         $(canvas).attr('height', height);
-        this.pen = new Pen();
-      }      
-    },
-    resetBoundary: function (x, y) {
-      if (x < minX) {
-        minX = x;
-      } else if (x > maxX) {
-        maxX = x;
-      }
-      if (y < minY) {
-        minY = y;
-      } else if (y > maxY) {
-        maxY = y;
+        pen.setContext(cntxt);
       }
     },
-    get: function () {
-      return {
-        minX: minX,
-        maxX: maxX,
-        minY: minY,
-        maxY: maxY
-      };
+    getHeight: function () {
+      return height;
+    },
+    getWidth: function () {
+      return width;
     },
     clear: function () {
       cntxt.fillStyle = 'rgb(255,255,255)';
@@ -151,22 +123,86 @@ var Board = (function () {
   });
 }());
 
+var Boundary = (function () {
+  var minX = 0,
+    maxX = 0,
+    minY = 0,
+    maxY = 0;
+
+  return Class.Create({
+    get: function () {
+      return {
+        minX: minX,
+        maxX: maxX,
+        minY: minY,
+        maxY: maxY
+      };
+    },
+    getHeight: function () {
+      return (maxY - minY);
+    },
+    getWidth: function () {
+      return (maxX - minX);
+    },
+    getMinX: function () {
+      return minX;
+    },
+    getMaxX: function () {
+      return maxX;
+    },
+    getMinY: function () {
+      return minY;
+    },
+    getMaxY: function () {
+      return maxY;
+    },
+    set: function (obj) {
+      if (obj) {
+        minX = obj.minX;
+        maxX = obj.maxX;
+        minY = obj.minY;
+        maxY = obj.maxY;
+      }
+    },
+    setMinX: function (x) {
+      if (x < minX) {
+        minX = x;
+      }
+    },
+    setMaxX: function (x) {
+      if (x > maxX) {
+        maxX = x;
+      }
+    },
+    setMinY: function (y) {
+      if (y < minY) {
+        minY = y;
+      }
+    },
+    setMaxY: function (y) {
+      if (y > maxY) {
+        maxY = y;
+      }
+    }
+  });
+}());
+
 var Record = (function () {
-  var addEvents = 
+  var addEvents =
     function () {
       var getCoordinate =
         (function (X, Y) {
-          var x =X, y=Y;
+          var x = X, y = Y;
 
           return function (e) {
             var movementX = e.movementX ||
-                e.mozMovementX ||
-                e.webkitMovementX ||
-                0,
+              e.mozMovementX ||
+              e.webkitMovementX ||
+              0,
               movementY = e.movementY ||
-                e.mozMovementY ||
-                e.webkitMovementY ||
-                0;
+              e.mozMovementY ||
+              e.webkitMovementY ||
+              0;
 
             x += movementX;
             y += movementY;
@@ -182,8 +218,13 @@ var Record = (function () {
             y = pos.y;
 
           mousedown = true;
-          queue.push(x, y, 0);
-          board.resetBoundary(x, y);
+          if (canRecord) {
+            queue.push(x, y, 0);
+            boundary.setMinX(x);
+            boundary.setMaxX(x);
+            boundary.setMinY(y);
+            boundary.setMaxY(y);
+          }
           return false;
         },
         mouseMove = function (e) {
@@ -191,9 +232,12 @@ var Record = (function () {
             x = pos.x,
             y = pos.y;
 
-          if (mousedown) {
+          if (mousedown && canRecord) {
             queue.push(x, y, 1);
-            board.resetBoundary(x, y);
+            boundary.setMinX(x);
+            boundary.setMaxX(x);
+            boundary.setMinY(y);
+            boundary.setMaxY(y);
           }
           return false;
         },
@@ -209,46 +253,90 @@ var Record = (function () {
       doc.removeEventListener("mouseup", mouseUp, false);
       doc.addEventListener("mousedown", mouseDown, false);
       doc.addEventListener("mousemove", mouseMove, false);
-      doc.addEventListener("mouseup", mouseUp, false);    
+      doc.addEventListener("mouseup", mouseUp, false);
     },
+    normalise = function (X, Y) {
+      var 
+        ratioX = board.getWidth() / boundary.getWidth(),
+        ratioY = board.getHeight() / boundary.getHeight(),
+        boundaryMinX = boundary.getMinX(),
+        boundaryMinY = boundary.getMinY(),
+        x =
+        (
+          (
+            X + (boundaryMinX < 0 ? (-1 * boundaryMinX) : 0)
+          ) * ratioX
+        ),
+        y =
+        (
+          (
+            Y + (boundaryMinY < 0 ? (-1 * boundaryMinY) : 0)
+          ) * ratioY
+        );
+
+      return {
+        x: x,
+        y: y
+      };
+    },
+    canRecord = false,
     queue = null,
-    board = null;
+    board = null,
+    boundary = null;
 
   return Class.Create({
-    start: function () {
-      PointerLock.requestLock($(doc.body),
-        function () {          
-          queue = new Queue();
-          board = new Board();
+    init: function () {
+      queue = new Queue();
+      boundary = new Boundary();
+      board = new Board();
+    },
+    start: function (onStart, onStop) {
+      PointerLock.requestLock(
+        $(doc.body),
+        function () {
+          canRecord = true;
           queue.reset();
           addEvents();
-        });
+          if (typeof onStart === 'function') {
+            onStart();
+          }
+        },
+        this.stop.bind(this, onStop)
+        );
     },
     stop: function (onStop) {
-      cleanMouseEvents();
-      onStop();
+      canRecord = false;
+      if (typeof onStop === 'function') {
+        onStop();
+      }
     },
-    play: function (canvas_elm, recording, complete) {
-      queue = new Queue();
-      board = new Board(canvas_elm, recording.bound);
+    play: function (canvas_elm, recording, playComplete) {
+      boundary.set(recording.boundary);
+      board.set(canvas_elm, boundary.getHeight() / boundary.getWidth());      
       queue.set(recording.queue);
-      queue.forEachpop(function (x, y, type) {
-        switch (type) {
-          case 0:
-            board.pen.moveTo(x, y);
-            break;
-          case 1:
-            board.pen.lineTo(x, y);
-            break;
-          case 2:
-            complete();
-            break;
-        }
-      }.bind(this));
+      queue.forEachpop(
+        function (x, y, type) {
+          var pos = normalise(x, y);
+
+          switch (type) {
+            case 0:            
+              board.pen.moveTo(pos.x, pos.y);
+              break;
+            case 1:
+              board.pen.lineTo(pos.x, pos.y);
+              break;
+          }
+        }.bind(this),
+        function () {
+          if (typeof playComplete === 'function') {
+            playComplete();
+          }        
+        }.bind(this)
+      );
     },
     get: function () {
       return {
-        bound: board.get(),
+        boundary: boundary.get(),
         queue: queue.get()
       };
     },
--- assets/js/test/blackboard.js
+++ assets/js/test/blackboard.js
@@ -28,9 +28,9 @@ QUnit.module('Queue', {
     this.queue.reset();
   }
 });
-QUnit.asyncTest('push', function (assert) {
+QUnit.asyncTest('forEachpop', function (assert) {
   var pointList = [{x: 1, y: 2}, {x: 3, y: 4}, {x: 5, y: 6}],
-    intervals = [0, 500, 1000],
+    intervals = [500, 1000, 2000],
     temp_pointList = pointList.slice(),
     temp_intervals = intervals.slice(),
     loop = function (i, timeslot) {
@@ -41,61 +41,31 @@ QUnit.asyncTest('push', function (assert) {
         if (--i) {
           loop.call(this, i, temp_intervals.shift());
         } else {
-          timeTest.call(this);
+          play.call(this);
         }
       }.bind(this), timeslot);
     },
-    timeTest = function () {
-      var actionList = this.queue.get(),
-        point;
+    play = function () {
+      var lastTimeSlot = (new Date()).getTime();
 
-      for (var action in actionList) {
-        point = pointList.shift();
-        assert.equal(actionList[action].x, point.x);
-        assert.equal(Math.round(actionList[action].interval / 100) * 100,
-          intervals.shift());
-      }
-      QUnit.start();
+      assert.equal(this.queue.get().length, 3);
+      this.queue.forEachpop(function (x, y) {
+        var point = pointList.shift(),
+          presentTime = (new Date()).getTime(),
+          interval = presentTime - lastTimeSlot;
+
+        lastTimeSlot = presentTime;
+        assert.equal(x, point.x);
+        assert.equal(y, point.y);
+        assert.equal(Math.round(interval / 100) * 100, intervals.shift());
+      }.bind(this),
+      function() {
+        QUnit.start();
+      }.bind(this));
     };
+
   loop.call(this, temp_pointList.length, temp_intervals.shift());
 });
-//QUnit.asyncTest('forEachpop', function (assert) {
-//  var pointList = [{x: 1, y: 2}, {x: 3, y: 4}, {x: 5, y: 6}],
-//    intervals = [500, 1000, 2000],
-//    temp_pointList = pointList.slice(),
-//    temp_intervals = intervals.slice(),
-//    loop = function (i, timeslot) {
-//      setTimeout(function () {
-//        var point = temp_pointList.shift();
-//
-//        this.queue.push(point.x, point.y, 0);
-//        if (--i) {
-//          loop.call(this, i, temp_intervals.shift());
-//        } else {
-//          play.call(this);
-//        }
-//      }.bind(this), timeslot);
-//    },
-//    play = function () {
-//      var lastTimeSlot = (new Date()).getTime();
-//
-//      this.queue.forEachpop(function (x, y) {
-//        var point = pointList.shift(),
-//          presentTime = (new Date()).getTime(),
-//          interval = presentTime - lastTimeSlot;
-//
-//        lastTimeSlot = presentTime;
-//        assert.equal(x, point.x);
-//        assert.equal(y, point.y);
-//        assert.equal(Math.round(interval / 100) * 100, intervals.shift());
-//        if (!intervals.length) {
-//          QUnit.start();
-//        }
-//      }.bind(this));
-//    };
-//
-//  loop.call(this, temp_pointList.length, temp_intervals.shift());
-//});
 
 QUnit.module('Record', {
   beforeEach: function () {
@@ -146,7 +116,7 @@ QUnit.module('Record', {
     doc = document;
   }
 });
-QUnit.asyncTest('start', function (assert) {
+QUnit.asyncTest('play', function (assert) {
   var recording = {},
     height = 0,
     queue_check = [
@@ -197,10 +167,10 @@ QUnit.asyncTest('start', function (assert) {
     assert.equal(recording.queue[k].y, q.y);
     assert.equal(recording.queue[k].type, q.type);
   }  
-  assert.equal(recording.bound.minX, 0);
-  assert.equal(recording.bound.maxX, 10);
-  assert.equal(recording.bound.minY, -10);
-  assert.equal(recording.bound.maxY, 0);
+  assert.equal(recording.boundary.minX, 0);
+  assert.equal(recording.boundary.maxX, 10);
+  assert.equal(recording.boundary.minY, -10);
+  assert.equal(recording.boundary.maxY, 0);
 
   this.width = function () {
     return 5;
@@ -220,45 +190,6 @@ QUnit.asyncTest('start', function (assert) {
     recording,
     function () {
       assert.equal(height, 5);
-      this.start();
-    }.bind(QUnit));
-});
-QUnit.asyncTest('play', function (assert) {
-  var bound = {
-    minX: -4,
-    maxX: 196,
-    minY: -2,
-    maxY: 98
-  },
-  queue = [
-    {x: -4, y: -2, type: 0, interval: 0},
-    {x: 90, y: 40, type: 0, interval: 1}
-  ],
-    check_against = [
-      {
-        x: 0,
-        y: 0
-      },
-      {
-        x: 47,
-        y: 21
-      }
-    ];
-
-  this.width = function () {
-    return 100;
-  };
-  this.cntxt.moveTo = function (x, y) {
-    var point = check_against.shift();
-
-    assert.equal(x, point.x);
-    assert.equal(y, point.y);
-  };
-  this.record.play(true,
-    {
-      bound: bound,
-      queue: queue
-    }, function () {
-      this.start();
-    }.bind(QUnit));
+      QUnit.start();
+    });
 });
